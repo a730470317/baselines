7deaa80c167cb5ce2e3d26c91ff7dbacfe4b4fe0
ziv.lin <730470317@qq.com>
2019-02-12 18:35:43 +0800

    [Add] update trainning parameter

--- baselines/run_crossgap.bat
+++ baselines/run_crossgap.bat
@@ -1,2 +1,2 @@
 rm ./save_img/*
-python run.py --alg=trpo_mpi --env=Crossgap-v2 --network=pretrain --seed=0 --num_timesteps=2e12 --timesteps_per_batch=10000 --max_kl=0.001 --nminibatches=2 --cg_iters=50
+python run.py --alg=trpo_mpi --env=Crossgap-v2 --network=pretrain --seed=0 --num_timesteps=2e12 --timesteps_per_batch=10000 --max_kl=0.0001 --nminibatches=1 --cg_iters=50  --load_path="D:/training_log/baseline_trpo/last"
